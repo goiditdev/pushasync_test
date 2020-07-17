5184c2679f84c904914b77dd34979a76b9d4c134
Michael Hempel <michael@goidit.net>
2020-07-17 14:45:02 -0500

    created different tests

--- pushasync_test/MainPage.xaml.cs
+++ pushasync_test/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,74 @@ namespace pushasync_test
             InitializeComponent();
         }
 
-        async void Handle_RunTest(object sender, EventArgs e)
+        async void Handle_RunTest_AwaitedPush(object sender, EventArgs e)
         {
-
-            Page p = new SecondPage();
-            await this.Navigation.PushAsync(p);
+            try
+            {
+                Page p = new SecondPage();
+                System.Console.WriteLine("Running the test for awaited push...");
+                Acr.UserDialogs.UserDialogs.Instance.Alert("This is a test", "Testing...");
+                await this.Navigation.PushAsync(p);
+                System.Console.WriteLine("Returned from awaited PushAsync, checking if page is on stack");
+                if (this.Navigation.NavigationStack.Contains(p))
+                {
+                    System.Console.WriteLine("Page was pushed to stack!");
+                }
+                else
+                {
+                    System.Console.WriteLine("Page is not on stack!");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+        }
+        async void Handle_RunTest_UnawaitedPush(object sender, EventArgs e)
+        {
+            try
+            {
+                Page p = new SecondPage();
+                System.Console.WriteLine("Running the test for unawaited push...");
+                Acr.UserDialogs.UserDialogs.Instance.Alert("This is a test", "Testing...");
+                this.Navigation.PushAsync(p);
+                System.Console.WriteLine("Returned from unawaited PushAsync, checking if page is on stack");
+                if (this.Navigation.NavigationStack.Contains(p))
+                {
+                    System.Console.WriteLine("Page was pushed to stack!");
+                }
+                else
+                {
+                    System.Console.WriteLine("Page is not on stack!");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+        }
+        async void Handle_RunTest_AwaitedAlert(object sender, EventArgs e)
+        {
+            try
+            {
+                Page p = new SecondPage();
+                System.Console.WriteLine("Running the test for awaited AlertAsync...");
+                await Acr.UserDialogs.UserDialogs.Instance.AlertAsync("This is a test", "Testing...");
+                await this.Navigation.PushAsync(p);
+                System.Console.WriteLine("Returned from awaited PushAsync, checking if page is on stack");
+                if (this.Navigation.NavigationStack.Contains(p))
+                {
+                    System.Console.WriteLine("Page was pushed to stack!");
+                }
+                else
+                {
+                    System.Console.WriteLine("Page is not on stack!");
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
