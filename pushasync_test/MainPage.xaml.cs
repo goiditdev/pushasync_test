4a0c41827df9c47964309dfc0f4151593e48c8cb
Michael Hempel <michael@goidit.net>
2020-07-17 13:40:32 -0500

    first commit

---
+++ pushasync_test/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace pushasync_test
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_RunTest(object sender, EventArgs e)
+        {
+
+            Page p = new SecondPage();
+            await this.Navigation.PushAsync(p);
+        }
+    }
+}
